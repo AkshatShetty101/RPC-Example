588ff52b442a8790c62d9231cb54bc4a86e7133a
AkshatShetty101 <akshatshetty2908@gmail.com>
2018-04-16 21:28:24 +0530

    Finished master conn and processing requests

--- slaveServer.js
+++ slaveServer.js
@@ -1,20 +1,62 @@
 const net = require('net'),
     JsonSocket = require('json-socket');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const uuid = require('uuid/v4')
+const app = new Koa();
+const router = new Router();
 let myArgs = process.argv.slice(2);
 console.log('myArgs: ', myArgs);
+let activeProcesses = {};
+let output = {};
 if (myArgs.length >= 1) {
+
     let port = myArgs[0];
+    app.use(bodyParser());
+
+    router.get('/status',(ctx,next)=>{
+        console.log('Retrieving slave status');
+        ctx.body = {
+            activeProcesses:activeProcesses
+        };
+    });
+
+    router.get('/', (ctx, next) => {
+        console.log('Finding output');
+        if (output[ctx.query.id]) {
+            ctx.body = output[ctx.query.id]
+        } else {
+            ctx.body = { status: "-1", message: 'No such process' };
+        }
+    });
+
+    app.use(router.routes())
+        .use(router.allowedMethods());
+
+    app.listen((Number(port) + 1), () => {
+        console.log('Slave up and running!');
+    });
+
     let server = net.createServer();
     server.listen(port);
     server.on('connection', function (socket) { //This is a standard net.Socket
         socket = new JsonSocket(socket); //Now we've decorated the net.Socket to be a JsonSocket
         console.log('Master established connection!');
         socket.on('message', function (message) {
-            console.log(message);
-            let result = message.a + message.b;
-            socket.sendEndMessage({ result: result });
+            if (message.type == 1) {
+                let result = { id: uuid(),port: Number(port), count: Object.keys(activeProcesses).length };
+                //Send number of working processes!
+                console.log(result);
+                socket.sendMessage(result);
+            } else if (message.type == 2) {
+                activeProcesses[message.id] = { status: 0, output: "Still processing!" };
+                output[message.id] = { status: 0, output: "Still processing!" };
+                runSlowProcess(message.id, message.data);
+                console.log('Processing!');
+            }
         });
-        socket.on('error',connectionErrorHandler);
+        socket.on('error', connectionErrorHandler);
     });
 } else {
     console.log('Port not specifed!');
@@ -22,4 +64,21 @@ if (myArgs.length >= 1) {
 
 function connectionErrorHandler(data) {
     console.log('Master terminated the connection!');
+}
+
+function runSlowProcess(id, data) {
+    setTimeout(() => {
+        output[id] = {
+            status: 1,
+            output: {
+                sum: (data.a + data.b),
+                difference: (data.a - data.b),
+                product: (data.a * data.b),
+                quotient: (data.a / data.b),
+                remainder: (data.a % data.b)
+            }
+        }
+        delete activeProcesses[id];
+
+    }, 25000)
 }
